818f43982118161f81f01f891e2f39360da5bb8e
Owen <ovand2@ocdsb.ca>
2021-09-17 12:44:55 -0400

    Added function calls to AddressBook class for adding/removing people from the book.

---
+++ src/AddressBook.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> buddies = new ArrayList<>();
+
+    public void addBuddy(BuddyInfo buddy){
+        buddies.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        buddies.remove(buddy);
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("address Book");
+
+    }
+}
